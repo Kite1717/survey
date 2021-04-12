122450f2c593328efc5d8f88ee17ff4864fca9c0
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2021-04-13 00:43:28 +0300

    dfgdfgh

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import "core-js";
+import "raf/polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
-
-import "core-js";
-import "raf/polyfill";
 
 ReactDOM.render(
   <React.StrictMode>
