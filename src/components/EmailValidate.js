920c5d33caf08a8bee668515dde2831cc1eb6bc5
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2021-04-12 21:55:44 +0300

    gffgdg

--- src/components/EmailValidate.js
+++ src/components/EmailValidate.js
@@ -130,15 +130,15 @@ function EmailValidate({
                     const errors = {};
                     if (!values.email) {
                       errors.email = "E posta adresiniz boş olamaz.";
-                    } 
-                    else if (
-                      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
-                        values.email
-                      )
-                    ) {
-                      errors.email =
-                        "Lütfen geçerli bir e-posta adresi giriniz.";
                     }
+                    // else if (
+                    //   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                    //     values.email
+                    //   )
+                    // ) {
+                    //   errors.email =
+                    //     "Lütfen geçerli bir e-posta adresi giriniz.";
+                    // }
                     return errors;
                   }}
                   onSubmit={(values, { setSubmitting }) => {
