645fc21f3cfa4665434e218c40789d5591aad021
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2021-04-12 21:50:37 +0300

    gffgdg

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "babel-polyfill";
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 
